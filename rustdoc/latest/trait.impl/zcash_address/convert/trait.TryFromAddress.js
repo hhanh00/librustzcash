62d123a1f7e1acd43037011c48070af72c3c6d2f
hhanh00 <hhanh00@users.noreply.github.com>
2025-05-17 11:20:30 +0000

    deploy: 0b677f2f55298896067d26c6cf91a8af902f9cda

--- rustdoc/latest/trait.impl/zcash_address/convert/trait.TryFromAddress.js
+++ rustdoc/latest/trait.impl/zcash_address/convert/trait.TryFromAddress.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["zcash_address",[]],["zcash_inspect",[["impl TryFromAddress for <a class=\"struct\" href=\"zcash_inspect/address/struct.Address.html\" title=\"struct zcash_inspect::address::Address\">Address</a>"]]]]);
+    var implementors = Object.fromEntries([["zcash_address",[]],["zcash_keys",[["impl TryFromAddress for <a class=\"enum\" href=\"zcash_keys/address/enum.Address.html\" title=\"enum zcash_keys::address::Address\">Address</a>"]]],["zcash_transparent",[["impl TryFromAddress for <a class=\"enum\" href=\"zcash_transparent/address/enum.TransparentAddress.html\" title=\"enum zcash_transparent::address::TransparentAddress\">TransparentAddress</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[20,180]}
+//{"start":57,"fragment_lengths":[20,165,219]}
