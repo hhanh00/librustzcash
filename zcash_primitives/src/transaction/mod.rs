938f1ca916651ba3e94b9a584ab93b7c8d5b3cae
Hanh Huynh Huu <hanh425@gmail.com>
2026-06-05 15:38:17 +0800

    fix: add missing issue_bundle field to TransactionData Clone impl

--- zcash_primitives/src/transaction/mod.rs
+++ zcash_primitives/src/transaction/mod.rs
@@ -497,6 +497,8 @@ impl Clone for TransactionData<Authorized> {
             sprout_bundle: self.sprout_bundle.clone(),
             sapling_bundle: self.sapling_bundle.clone(),
             orchard_bundle: self.orchard_bundle.clone(),
+            #[cfg(zcash_unstable = "nu7")]
+            issue_bundle: self.issue_bundle.clone(),
             #[cfg(zcash_unstable = "zfuture")]
             tze_bundle: self.tze_bundle.clone(),
         }
